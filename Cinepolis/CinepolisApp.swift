96deda13fe874fbb2f36bfe16ae88c1cc050048f
GerardoValencia <gerardo.valencia@iest.edu.mx>
2023-10-13 22:29:38 -0600

    First Commit
    Actividad 7: Menú Cinépolis

---
+++ Cinepolis/CinepolisApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinepolisApp.swift
+//  Cinepolis
+//
+//  Created by Gerardo Valencia Rodríguez on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CinepolisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
